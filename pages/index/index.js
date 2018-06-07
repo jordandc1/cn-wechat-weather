7c3271f6d4327feb0591f4ac6ae6ec9dd4583a29
jordandc1 <289483489@qq.com>
2018-06-07 18:17:11 +0800

    request added

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,75 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const imageMap = {
+  'sunny': '/pages/image/sunny-bg.png',
+  'cloudy': '/pages/image/cloudy-bg.png',
+  'overcast': '/pages/image/overcast-bg.png',
+  'lightrain': '/pages/image/lightrain-bg.png',
+  'heavyrain': '/pages/image/heavyrain-bg.png',
+  'snow': '/pages/image/snow-bg.png'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+
+Page({
+  data: {
+    temp: '',
+    weather: '',
+    url: ''
+  },
+  onLoad() {
+    this.getNow()
+  },
+
+  onPullDownRefresh: function () {
+    console.log("refresh executed!")
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          temp: temp + '°',
+          weather: weatherMap[weather],
+          url: imageMap[weather]
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+
+      },
+      complete: () => {
+        callback && callback() //If the left of the && is truth-y, then whatever is on the right side of the && is executed.
+        // 如果有回调参数 则执行
+      }
+    })
+  }
+
+})
